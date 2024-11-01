1ae206ad9dc907039f9de709246e78d676264387
Jtepssss <mateoelgamer125@gmail.com>
2024-11-01 12:44:13 -0500

    configuracion-server

--- src/Index.ts
+++ src/Index.ts
@@ -4,7 +4,13 @@ import cors from "cors" ;
 
 import { AppDataSource } from "./data-source";
 
-import swaggerUi from "swagger-ui-express" ;
+import router from "./routers/productRoutes";
+
+ import swaggerUI from  "swagger-ui-express"
+
+ import swaggerSpec from "./swagger/swagger";
+
+
 
 const app : Application = express();
 const PORT = process.env.PORT ?? 3000 ;
@@ -13,4 +19,28 @@ const PORT = process.env.PORT ?? 3000 ;
 
 app .use (cors());
 app.use (express.json()) ;
+
+//Rutas
+
+app.use ("/api/",router) ; // Ruta de Productos 
+
+//Documentacion del swagger 
+
+app.use("/api-docs", swaggerUI.serve , swaggerUI.setup(swaggerSpec));
+
+//inicializacion de la base de datos y servidor 
+
+AppDataSource.initialize()
+    .then( () => {
+        app.listen(() => {
+            console.log(` Servidor corriendo en  http://localhost${PORT}\n`);
+            console.log(` Endpoints:`);
+            console.log(`API Products http://Localhost:${PORT}/api/products\n`);
+            console.log(`Docuemntacion:`);
+            console.log(`swagger en http://Localhost${PORT}/api-docs`);
+            
+        });
+    } )
+    .catch( (error) => console.log(error) );
+
 
